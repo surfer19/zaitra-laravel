333d47c102fee2bdfec354d839b6f69055d9221d
Marian <marianmrva123@gmail.com>
2020-08-17 14:58:47 +0200

    Remove commented js

--- public/js/app.js
+++ public/js/app.js
@@ -120,32 +120,7 @@ function activateNavBg(scrollpos, header_height) {
   } else {
     remove_class_on_scroll();
   }
-} // Closes the sidebar menu
-// $("#menu-close").click(function(e) {
-//     e.preventDefault();
-//     $("#sidebar-wrapper").toggleClass("active");
-//   });
-//   // Opens the sidebar menu
-//   $("#menu-toggle").click(function(e) {
-//     e.preventDefault();
-//     $("#sidebar-wrapper").toggleClass("active");
-//   });
-// Scrolls to the selected menu item on the page
-// $(function() {
-//     $('a[href*=#]:not([href=#])').click(function() {
-//         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
-//         var target = $(this.hash);
-//         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-//         if (target.length) {
-//             $('html,body').animate({
-//             scrollTop: target.offset().top
-//             }, 1000);
-//             return false;
-//         }
-//         }
-//     });
-// });
-// Map scrolling behaviour
+} // Map scrolling behaviour
 
 
 $(document).ready(function () {
