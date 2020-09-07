1f682837bb430243babdb0618a3b08c843c35f6c
Martin Javorka <martin.javorka.ml@gmail.com>
2020-09-07 16:28:02 +0200

    Refactor navbar links

--- resources/views/shared/navigation.blade.php
+++ resources/views/shared/navigation.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-zaitra navbar-expand-lg navbar-dark fixed-top" id="menu-toggle">
-    <a class="navbar-brand" href="#">
-        <img src="{{ url('images/zaitra-logo.svg') }}" width="40" height="40" alt="">
+    <a class="navbar-brand" href="{{ route('home') }}">
+        <img src="{{ asset('images/zaitra-logo.svg') }}" width="40" height="40" alt="">
     </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -8,13 +8,13 @@
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav ml-auto">
       <li class="nav-item {{ Request::is('/') ? 'active' : '' }}">
-        <a class="nav-link" href="{{ url('/') }}">HOME <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="{{ route('home') }}">HOME <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item {{ Request::is('products') ? 'active' : '' }}">
-        <a class="nav-link" href="{{ url('/products') }}">PRODUCTS</a>
+        <a class="nav-link" href="{{ route('products') }}">PRODUCTS</a>
       </li>
       <li class="nav-item {{ Request::is('services') ? 'active' : '' }}">
-        <a class="nav-link" href="{{ url('/services') }}">SERVICES</a>
+        <a class="nav-link" href="{{ route('services') }}">SERVICES</a>
       </li>
     </ul>
   </div>
