0444fb1c5da630ade3f3e00906f118b1696dc5f7
Marian <marianmrva123@gmail.com>
2020-09-09 15:15:46 +0200

    Update home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     @include('home.header')
     @include('home.who-we-are')
+    @include('home.products-services')
     @include('home.partners')
-    @include('home.about')
     {{-- todo content to news section --}}
     {{-- @include('home.news') --}}
 @endsection
