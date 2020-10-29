151deeeabeb166eca41cf4bfdcee7b582803e315
Marek Marušin <marusin.marek@gmail.com>
2020-10-29 10:31:59 +0100

    finalize news section for production

--- resources/lang/en/home.php
+++ resources/lang/en/home.php
@@ -19,10 +19,12 @@ return [
     of the process. Does your company need help to automate Machine Learning pipelines or  Earth Observation data
     processing? We have helped our customers to design and deploy their applications to modern cloud environments
     and build custom software solutions to optimize their processes.',
-    'news.copernicus_accelerator.title' => 'Zaitra is listed in Copernicus accelerator portfolio',
-    'news.copernicus_accelerator.description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vehicula nisi et tortor venenatis vestibulum. Nulla facilisi. In hac habitasse platea dictumst. In fringilla at tortor ut faucibus. Nulla nec est in mi aliquet accumsan.',
-    'news.dobre_noviny.title' => 'Dobré noviny - Chlapci spod Tatier dokážu odsledovať výskyt lykožrútov aj nelegálnu ťažbu',
-    'news.dobre_noviny.description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vehicula nisi et tortor venenatis vestibulum. Nulla facilisi. In hac habitasse platea dictumst. In fringilla at tortor ut faucibus. Nulla nec est in mi aliquet accumsan. In hac habitasse platea dictumst. In fringilla at tortor ut faucibus. Nulla nec est in mi aliquet accumsan.',
-    'news.hospodarske_noviny.title' => 'Hospodářské noviny - Najdeme kůrovce či slevy v okolí, propojíme satelity. Kosmická agentura podpořila další start-upy',
-    'news.hospodarske_noviny.description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vehicula nisi et tortor venenatis vestibulum. Nulla facilisi. In hac habitasse platea dictumst. In fringilla at tortor ut faucibus. Nulla nec est in mi aliquet accumsan. In hac habitasse platea dictumst. In fringilla at tortor ut faucibus. Nulla nec est in mi aliquet accumsan.',
+    'news.copernicus_accelerator.title' => 'Zaitra is listed in Copernicus Accelerator portfolio (EN)',
+    'news.copernicus_accelerator.description' => 'If you work with Earth observation (EO) data, you know how many challenges (which are really just potential opportunities) there are to improve current environmental monitoring and planning techniques. When combined with artificial intelligence (AI), the tools that can be created are game-changing...',
+    'news.hospodarske_noviny.title' => 'ESA supported new startups, Zaitra is among them! (CZ)',
+    'news.hospodarske_noviny.description' => 'Developers from Zaitra are developing smart forest inventory based on satellite imagery and artificial intelligence technology. They are helping forest managers and researchers...',
+    'news.startup_jobs.title' => 'Early alerts for vegetation risks are helping energy distribution companies (CZ)',
+    'news.startup_jobs.description' => 'Czech innovation company is developing AI pipelines for optimization results mined from earth observation data and providing added value information for industries that deal with vegetation...',
+    'news.svet_chytre.title' => 'Zaitra fights against forest degradation by using satellites (CZ)',
+    'news.svet_chytre.description' => 'By combining satellite data with AI/ML algorithms we are able to detect events that correspond to significant degradation of forest health such as clearings, dead trees, loss of vegetation, dry areas, etc...',
 ];
--- resources/views/home/news.blade.php
+++ resources/views/home/news.blade.php
@@ -8,6 +8,30 @@
         <div class="row">
             <div class="col-12">
                 <ul class="list-unstyled">
+                    <a href="https://www.startupjobs.cz/newsroom/chytra-inventura-od-zaitra-ma-pomahat-lesnikum-druzicove-snimky-vcas-upozorni-na-sucho-i-skudce" class="media-link" target="_blank">
+                        <li class="media align-items-center my-4">
+                            <img class="mr-3" src="{{ url('images/startup-jobs.svg') }}" width="100" alt="Dobre noviny image">
+                            <div class="media-body">
+                                <h3 class="mt-0 mb-1">@lang('home.news.startup_jobs.title')</h3>
+                                <p>19.10.2020</p>
+                                <p>
+                                    @lang('home.news.startup_jobs.description')
+                                </p>
+                            </div>
+                        </li>
+                    </a>
+                    <a href="https://www.svetchytre.cz/a/pE2Nq/druzicemi-proti-kurovci-jak-brnensky-startup-zachranuje-lesy" class="media-link" target="_blank">
+                        <li class="media align-items-center my-4">
+                            <img class="mr-3" src="{{ url('images/svet-chytre.svg') }}" width="100" alt="Dobre noviny image">
+                            <div class="media-body">
+                                <h3 class="mt-0 mb-1">@lang('home.news.svet_chytre.title')</h3>
+                                <p>09.10.2020</p>
+                                <p>
+                                    @lang('home.news.svet_chytre.description')
+                                </p>
+                            </div>
+                        </li>
+                    </a>
                     <a href="https://accelerator.copernicus.eu/portfolio/zaitra/" class="media-link" target="_blank">
                         <li class="media align-items-center">
                             <img class="mr-3" src="{{ url('images/copernicus-accelerator.svg') }}" width="100" alt="Copernicus accelerator logo">
@@ -20,18 +44,6 @@
                             </div>
                         </li>
                     </a >
-                    <a href="https://www.dobrenoviny.sk/c/189562/chlapci-spod-tatier-mapovat-vyskyt-lykozrutov-a-nasledky-sucha" class="media-link" target="_blank">
-                        <li class="media align-items-center my-4">
-                            <img class="mr-3" src="{{ url('images/dobre-noviny.png') }}" width="100" alt="Dobre noviny image">
-                            <div class="media-body">
-                                <h3 class="mt-0 mb-1">@lang('home.news.dobre_noviny.title')</h3>
-                                <p>09.09.2020</p>
-                                <p>
-                                    @lang('home.news.dobre_noviny.description')
-                                </p>
-                            </div>
-                        </li>
-                    </a>
                     <a href="https://archiv.ihned.cz/c1-66801140-najdeme-kurovce-ci-slevy-v-okoli-propojime-satelity-kosmicka-agentura-podporila-dalsi-start-upy" class="media-link" target="_blank">
                         <li class="media align-items-center my-4">
                             <img class="mr-3" src="{{ url('images/hospodarske-noviny.svg') }}" width="100" alt="Hospodarske noviny logo">
